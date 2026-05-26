cfd2e9d3992301adaa4548b47a770287bfeda3ba
tadalvn <justtada9@gmail.com>
2026-05-26 21:28:09 +0700

    Tambah file Kondisi.js

---
+++ kondisi.js
@@ -0,0 +1,11 @@
+const nilai = 90;
+
+if (nilai >= 85) {
+  console.log("Nilai Kamu adalah A");
+} else if (nilai >= 75) {
+  console.log("Nilai Kamu adalah AB");
+} else if (nilai >= 65) {
+  console.log("Nilai Kamu adalah B");
+} else {
+  console.log("Nilai kamu remidi");
+}
